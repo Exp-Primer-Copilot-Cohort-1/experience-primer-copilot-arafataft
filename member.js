56fe4d15c53ddd3b8768c4dd4ab1a19ee5e85029
arafataft <arafatcd@gmail.com>
2023-12-28 00:12:35 +0600

    commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
 function skillsMember(){
-    return memberInfo;
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "black";
+    member.style.backgroundColor = "white";
+    member.style.borderRadius = "0px";
+    member.style.border = "1px solid black";
 }
